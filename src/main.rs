2d7319500581065d57cff356a0673ae717ac7478
Frank Jimenez <jjcfrank@gmail.com>
2024-04-02 16:42:37 +0100

    improved workflow

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
-use clap::Parser;
-use std::path::Path;
+use clap::{ArgAction, Parser};
+use io::to_geojson;
 use std::env;
+use std::path::{Path, PathBuf};
 mod database;
 mod io;
 mod utils;
-
 
 /// Get polygons from OSM water that intersect with the target geometries and output results in GeoJSON.
 #[derive(Parser, Debug)]
 #[command(author = "jjcfrancisco", version = "0.1.1", about, long_about = None)]
 struct Cli {
-    /// Connection string to a database if using SQL as target
-    #[arg(long)]
-    uri: Option<String>,
-
     /// Filepath to GeoJSON, Shapefile or SQL
     #[arg(long)]
     target: String,
 
-    /// Filepath to OSM water shapefile
-    #[arg(long)]
-    input: String,
-
     /// Filepath to save output file
     #[arg(short, long)]
     output: String,
+
+    ///// Connection string to a database if using SQL as target
+    //#[arg(long)]
+    //uri: Option<String>,
+
+    /// Filepath to OSM water shapefile
+    #[arg(long)]
+    water: Option<String>,
+
+    /// Coordinate system
+    #[arg(long, default_value = "4326")]
+    srid: Option<String>,
+
+    /// Download water
+    #[arg(
+        long,
+        action(ArgAction::SetTrue),
+        default_value = "false",
+        required_unless_present = "water"
+    )]
+    download: Option<bool>,
 }
 
 fn main() {
     let args = Cli::parse();
 
     // args need better parsing
+    //let uri: Option<String> = args.uri;
     let target: String = args.target;
-    let input: String = args.input;
+    let water: Option<String> = args.water;
     let output: String = args.output;
-    let uri: Option<String> = args.uri;
+    let srid: Option<String> = args.srid;
+    let download: Option<bool> = args.download;
 
     // Set path to current dir
     let result = env::set_current_dir(Path::new("./"));
@@ -45,9 +60,22 @@ fn main() {
 
     // Workflow
     if utils::check_provided_output(&output) {
-        let target_geom = io::open(&target, uri);
-        let osm_water_polys = io::read_shapefile(&input);
-        let result = utils::geom_intersects(osm_water_polys, target_geom);
-        io::to_geojson(&output, result)
+        let target_geom = io::open_target(&target);
+        let water_data = match download {
+            Some(b) => if b {
+                io::download_unzip_read(srid)
+            } else {
+                io::open_shapefile(PathBuf::from(water.expect("Error unpacking 'water' flag.")))
+            },
+            None => io::open_shapefile(PathBuf::from(water.expect("Error unpacking 'water' flag."))),
+        };
+        let output_data = match water_data {
+            Ok(wd) => utils::geom_intersects(wd, target_geom),
+            Err(e) => panic!("Error finding geometry intersects: {}", e)
+        };
+        match output_data {
+            Some(od) => to_geojson(&output, od),
+            None => println!("No water bodies intersected with the target geometries")
+        };
     }
 }
